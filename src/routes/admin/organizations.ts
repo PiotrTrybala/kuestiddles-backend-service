876fac059cb56de13910a83c45982827edd9f1f8
PiotrTrybala <trybalapiotr4@gmail.com>
2026-02-22 18:05:22 +0100

    implemented organizations router

--- src/routes/admin/organizations.ts
+++ src/routes/admin/organizations.ts
@@ -1,14 +1,27 @@
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 import { type AppEnv } from "../../config/app";
 import { landmarksRouter } from "./landmarks";
 import { questsRouter } from "./quests";
 import { assetsRouter } from "./assets";
 import { requireOrganization } from "./middleware";
+import { database } from "../../database/db";
+import { organizations } from "../../database/schema/organizations";
 
-export const organizationsRouter = new Hono<AppEnv>().basePath("/:orgName");
+export const organizationsRouter = new Hono<AppEnv>();
+
+
+organizationsRouter.get("/list", async (c) => {
+    const user = c.get("user")!;
+
+    const result = await database.select().from(organizations).where(eq(organizations.user_id, user.id));
+    return c.json({
+        organizations: result,
+    })
+});
 
 organizationsRouter.use("*", requireOrganization);
 
-organizationsRouter.route("/landmarks", landmarksRouter);
-organizationsRouter.route("/quests", questsRouter);
-organizationsRouter.route("/assets", assetsRouter);
+organizationsRouter.route("/:orgName/landmarks", landmarksRouter);
+organizationsRouter.route("/:orgName/quests", questsRouter);
+organizationsRouter.route("/:orgName/assets", assetsRouter);
