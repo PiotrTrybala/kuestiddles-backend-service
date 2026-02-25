1ceaa0cfed7cea7edd3666a4a863bb75b0d0fe44
PiotrTrybala <trybalapiotr4@gmail.com>
2026-02-25 19:34:02 +0100

    implemented auth avatar router

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -4,17 +4,26 @@ import sharp from "sharp";
 import { s3 } from "../../config/s3";
 import { database } from "../../database/db";
 import { user } from "../../database/schema/auth";
-
-const getAvatarPath = (name: string) => {
-    return `avatars/${name}`;
-};
+import { eq } from "drizzle-orm";
+import { defaultProfilePictureFile } from "../../static";
 
 export const authRouter = new Hono<AppEnv>();
 
 authRouter.get("/avatar/:id", async (c) => {
 
-    const userId = c.req.param("id");  
-    const profilePicturePath = getAvatarPath(`${userId}.webp`);
+    const userId = c.req.param("id");
+
+    const [result] = await database.select({ imageUrl: user.image }).from(user).where(eq(user.id, userId));
+
+    if (result?.imageUrl == "") {
+        return c.body(await defaultProfilePictureFile.arrayBuffer(), 200, {
+            'Content-Type': 'image/webp',
+            'Cache-Control': 'public, max-age=31536000'
+        });
+    }
+
+
+    const profilePicturePath = `avatars/${userId}.webp`;
 
     const picture = s3.file(profilePicturePath);
     if (!(await picture.exists())) {
@@ -31,51 +40,46 @@ authRouter.get("/avatar/:id", async (c) => {
 
 authRouter.post("/avatar", async (c) => {
 
-    const u = c.get("user")!;
+    const userId = c.get("user")!.id;
     const form = await c.req.parseBody({ all: true });
     const rawProfilePicture = form['profilePicture'];
 
-    if (!(rawProfilePicture instanceof File)) return c.json({ message: "Profile picture must be file"}, 400);
+    if (!(rawProfilePicture instanceof File)) return c.json({ message: "Profile picture must be file" }, 400);
 
-    let profilePicture = rawProfilePicture as File | undefined;
+    let profilePicture = rawProfilePicture as File;
 
-    if (!profilePicture) {
-        // TODO: default profile picture
-        return c.json({
-
-        })
+    if (profilePicture.size > 2 * 1024 * 1024) {
+        return c.json({ message: "Profile picture too large" }, 400);
     }
-    
+
     const arrayBuffer = await profilePicture!.arrayBuffer();
 
     const webpBuffer = await sharp(arrayBuffer)
         .webp({ quality: 50 })
         .resize(256, 256, { withoutEnlargement: true, withoutReduction: true })
         .toBuffer();
-    
-    const profilePictureName = `${user.id}.webp`;
-    const profilePicturePath = getAvatarPath(profilePictureName);
-    const profilePictureURL = `${process.env.AVATARS_URL!}/${u.id}`;
+
+    const profilePictureName = `${userId}.webp`;
+    const profilePicturePath = `avatars/${profilePictureName}`
+    const profilePictureURL = `${process.env.AVATARS_URL!}/${userId}`;
     // TODO: Add hash calculation later
 
     try {
         await s3.write(profilePicturePath, webpBuffer, {
             type: "image/webp"
         });
-    } catch(error) {
+    } catch (error) {
         console.error("error detected:", error);
-        return c.json({ message: `Could not upload profile picture: ${profilePictureName}`}, 400);
+        return c.json({ message: `Could not upload profile picture: ${profilePictureName}` }, 400);
     }
 
     try {
-
         await database.update(user).set({
             image: profilePictureURL,
         });
-
-    } catch(error) {
+    } catch (error) {
         console.error("error detected:", error);
-        return c.json({ message: `Could not upload profile picture: ${profilePictureName}`}, 400);
+        return c.json({ message: `Could not upload profile picture: ${profilePictureName}` }, 400);
     }
 
     return c.json({
---
+++ src/static.ts
@@ -0,0 +1,3 @@
+
+export let defaultThumbnailFile = Bun.file("static/default-thumbnail.webp");
+export let defaultProfilePictureFile = Bun.file("static/default-profile-picture.webp");
