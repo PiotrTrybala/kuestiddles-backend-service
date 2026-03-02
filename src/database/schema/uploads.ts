e5f956a4040366b9f0efc27d5f9a3f6a1f235ce1
PiotrTrybala <trybalapiotr4@gmail.com>
2026-03-02 13:12:00 +0100

    implemented assets repository

--- src/database/schema/uploads.ts
+++ src/database/schema/uploads.ts
@@ -1,6 +1,6 @@
 import { sql } from 'drizzle-orm';
 import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
-import { organization, user } from './auth';
+import { member, organization, user } from './auth';
 import { timestamps } from './utils';
 
 export const UPLOADS_LABELS = ['profile', 'asset'];
@@ -8,7 +8,7 @@ export const UPLOADS_LABELS = ['profile', 'asset'];
 export const uploads = pgTable('uploads', {
     id: uuid().primaryKey().defaultRandom(),
     organization_id: text().references(() => organization.id),
-    user_id: text().references(() => user.id),
+    member_id: text().references(() => member.id),
     name: text().notNull(),
     labels: text().array().default(sql`'{}'::text[]`),
     path: text().notNull(),
--- src/repositories/assets.ts
+++ src/repositories/assets.ts
@@ -1,23 +1,119 @@
+import { and, arrayOverlaps, eq, ilike } from "drizzle-orm";
+import { uploads } from "../database/schema/uploads";
+import { database } from "../database/db";
+import sharp from "sharp";
+import { sha256 } from "hono/utils/crypto";
+import { s3 } from "../config/s3";
+
+export type Asset = typeof uploads.$inferSelect;
 
 export type ListAssetsParams = {
     page: number,
     pageSize: number,
-    labels: string[],
     name: string,
+    labels: string[],
 };
 
-export async function listAssets(organizationId: string, params: ListAssetsParams) {
+export async function listAssets(organizationId: string, { page, pageSize, name, labels }: ListAssetsParams): Promise<{ assets: Asset[] }> {
 
+    const offset = page * pageSize;
+    const limit = pageSize;
+
+    const conditions = [
+        eq(uploads.organization_id, organizationId),
+    ];
+
+    if (name && name.trim() !== "") {
+        conditions.push(ilike(uploads.name, `%${name}%`));
+    }
+
+    if (labels.length > 0) {
+        conditions.push(arrayOverlaps(uploads.labels, labels));
+    }
+
+    const assets = await database.select()
+        .from(uploads)
+        .where(conditions.length > 0 ? and(...conditions) : undefined)
+        .offset(offset)
+        .limit(limit);
+
+    const fullAssets = assets.map((asset) => {
+        const url = `${process.env.ASSETS_URL!}/${asset.id}`;
+        return {
+            ...asset,
+            url,
+        };
+    });
+
+    return { assets: fullAssets };
 }
 
-export async function getAsset(id: string) {
+export async function getAsset(id: string): Promise<{ asset?: Asset, error?: string }> {
 
+    const asset = await database.query.uploads.findFirst({
+        where: eq(uploads.id, id)
+    });
+
+    if (!asset) {
+        return {
+            error: "Asset not found",
+        };
+    }
+
+
+    return {
+        asset,
+    }
 }
 
-export async function uploadAssets(organizationId: string, files: File[]) {
+export async function uploadAssets(
+    organizationId: string, 
+    memberId: string, 
+    assets: File[]
+): Promise<{ results?: {id: string, hash: string }[], error?: string }> {
 
+    try {
+        const uploadsResults = await Promise.all(assets.map(async (asset) => {
+            const buffer = await asset.arrayBuffer();
+
+            const webpBuffer = await sharp(Buffer.from(buffer))
+                .webp({ quality: 75 })
+                .resize(400, 300, { withoutEnlargement: true })
+                .toBuffer();
+
+            const assetHash = await sha256(webpBuffer);
+            const fileName = asset.name.split('.')[0];
+            const assetName = `${fileName}.webp`;
+            const hashedOrganizationId = await sha256(organizationId);
+            const assetPath = `assets/${hashedOrganizationId}/${assetName}`;
+
+            await s3.write(assetPath, webpBuffer, {
+                type: "image/webp",
+            });
+
+            const [metadata] = await database.insert(uploads).values({
+                member_id: memberId,
+                organization_id: organizationId,
+                name: assetName,
+                path: assetPath!,
+                hash: assetHash!,
+            }).returning();
+
+            if (!metadata) throw new Error(`Database failed for ${assetName}`);
+
+            return { id: metadata.id, hash: metadata.hash };
+        }));
+
+        return { results: uploadsResults };
+
+    } catch (error) {
+        console.error('Upload process failed:', error);
+        return {
+            error: error instanceof Error ? error.message : "An unknown error occurred during upload"
+        };
+    }
 }
 
 export async function deleteAsset(id: string) {
-
+    
 }
