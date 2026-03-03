3c8f1d9204ac5e2af8c82a48c2709bcc739cd310
PiotrTrybala <trybalapiotr4@gmail.com>
2026-03-03 15:52:02 +0100

    completed quests repo

--- src/repositories/quests.ts
+++ src/repositories/quests.ts
@@ -67,15 +67,32 @@ export type CreateQuestParams = {
     title: string,
     description: string,
     labels: string[],
-    thumbnail: string,
-    assets: string[],
+    thumbnail?: string,
+    assets?: string[],
     points: number,
 };
 
-export async function createQuest(organizationId: string, params: CreateQuestParams) { 
+export async function createQuest(organizationId: string, { landmarkId, title, description, labels, thumbnail, assets, points }: CreateQuestParams): Promise<{ quest?: Quest, error?: string }> {
     try {
+        const [quest] = await database.insert(quests).values({
+            organization_id: organizationId,
+            landmark_id: landmarkId,
+            title,
+            description,
+            labels,
+            thumbnail,
+            assets,
+            points,
+        }).returning();
 
-    } catch(error) {
+        if (!quest) {
+            return {
+                error: "Failed to create new quest",
+            }
+        }
+
+        return { quest }
+    } catch (error) {
         console.log('error detected:', error);
         return {
             error: "Internal error while creating new quest"
@@ -90,13 +107,70 @@ export type UpdateQuestParams = {
     }[]
 };
 
-export async function updateQuest(organizationId: string, params: UpdateQuestParams) {
+const ALLOWED_QUESTS_UPDATE_FIELDS = new Set([
+    "title",
+    "description",
+    "labels",
+    "thumbnail",
+    "assets",
+    "points",
+    "landmark",
+]);
+
+export async function updateQuest(id: string, params: UpdateQuestParams): Promise<{ quest?: Quest, error?: string }> {
+
+    try {
+        const updateData: any = {};
+
+        for (const update of params.updates) {
+
+            if (!ALLOWED_QUESTS_UPDATE_FIELDS.has(update.field)) return { error: `Field ${update.field} is not allowed or does not exists` };
+
+            if (update.field === "points") {
+                const points = update.value;
+                if (typeof points !== "number") return { error: "Points value is not a number" };
+
+                updateData.points = points;
+                continue;
+            }
+
+            updateData[update.field] = update.value;
+        }
+
+        if (Object.keys(updateData).length === 0) {
+            return { error: "No updates found" };
+        }
+
+        const [ updated ] = await database.update(quests)
+            .set(updateData)
+            .where(eq(quests.id, id))
+            .returning();
+
+        return { quest: updated };
+    } catch (error) {
+        console.log('error detected:', error);
+        return { error: "Internal error while updating quests" };
+    }
+}
+
+export async function deleteQuest(organizationId: string, id: string): Promise<{ error?: string }> {
+    try {
+
+        await database.delete(quests)
+            .where(and(
+                eq(quests.id, id),
+                eq(quests.organization_id, organizationId)
+            ));
+            
+        return {};
+    } catch(error) {
+        console.log('error detected:', error);
+        return { error: "Internal error while deleting quest" };
+    }
 
 }
 
-export async function deleteQuest(id: string) {
-
-}
+// USER METHODS: Used by /user router
 
 export async function solveQuest(questId: string, userId: string, answers: string[]) {
 
