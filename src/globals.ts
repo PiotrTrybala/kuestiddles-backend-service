bafd48ab215e09847773b929f8622abb127f7e8b
PiotrTrybala <trybalapiotr4@gmail.com>
2026-04-07 10:41:25 +0200

    [uploads] added new globals file

---
+++ src/globals.ts
@@ -0,0 +1,9 @@
+
+export const AVATARS_URL = process.env.AVATARS_URL!;
+export const IMAGES_URL = process.env.IMAGES_URL!;
+
+export const USER_API_URL = process.env.USER_API_URL!;
+export const ADMIN_API_URL = process.env.ADMIN_API_URL!;
+export const MOBILE_API_URL = process.env.MOBILE_API_URL!;
+
+export const APP_NAME = process.env.APP_NAME!;
