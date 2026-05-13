f57572045f5021cb7a8e858e04c34d6a9b89881d
PiotrTrybala <trybalapiotr4@gmail.com>
2026-05-13 22:11:26 +0200

    [payments] implemented payments router

--- src/globals.ts
+++ src/globals.ts
@@ -10,3 +10,6 @@ export const IMAGES_URL = process.env.IMAGES_URL!;
 export const USER_API_URL = process.env.USER_API_URL!;
 export const ADMIN_API_URL = process.env.ADMIN_API_URL!;
 export const MOBILE_API_URL = process.env.MOBILE_API_URL!;
+
+export const STRIPE_SUCCESS_URL = process.env.STRIPE_SUCCESS_URL!;
+export const STRIPE_CANCEL_URL = process.env.STRIPE_CANCEL_URL!;
--- src/routes/admin/v3/payments.ts
+++ src/routes/admin/v3/payments.ts
@@ -1,19 +1,57 @@
 import type { AppEnv } from "@/config/app";
+import { auth } from "@/config/auth";
+import { STRIPE_CANCEL_URL, STRIPE_SUCCESS_URL } from "@/globals";
+import { getUserQuotas } from "@/repositories/v3/payments";
 import { requireAuth } from "@/routes/middleware";
 import { Hono } from "hono";
 
 export const paymentsRouter = new Hono<AppEnv>();
 
+paymentsRouter.get("/", requireAuth("admin"), async (c) => {
+
+    const user = c.get("user")!;
+
+    const subscriptions = await auth.api.listActiveSubscriptions({
+        headers: c.req.raw.headers,
+    });
+
+    const activeSubscription = subscriptions.find(sub => sub.status === "active");
+
+    const { quotas, error } = await getUserQuotas(user.id);
+    if (error || !quotas) {
+        return c.json({ message: "Quotas has not been found" }, 404);
+    }
+
+    const plans = auth.options.plugins?.flatMap((p: any) => p.subscription?.plans ?? []);
+    const activePlan = plans?.find((p: any) => p.priceId === activeSubscription?.priceId);
+
+    return c.json({
+        subscription: activeSubscription ?? null,
+        plan: activePlan ?? null,
+        quotas,
+    });
+});
+
 type UpgradeSubscriptionPayload = {
-    
+    plan: string,
 };
 
 paymentsRouter.post("/upgrade", requireAuth("admin"), async (c) => {
+    const { plan } = await c.req.json<UpgradeSubscriptionPayload>();
 
+    const data = await auth.api.upgradeSubscription({
+        headers: c.req.raw.headers,
+        body: {
+            plan,
+            successUrl: STRIPE_SUCCESS_URL ?? null,
+            cancelUrl: STRIPE_CANCEL_URL ?? null,
+        }
+    });
 
+    if (data?.url) {
+        return c.redirect(data?.url);
+    }
 
+    return c.json({ error: "No redirect URL returned" }, 400);
 });
 
-paymentsRouter.get("/", requireAuth("admin"), async (c) => {
-
-});
