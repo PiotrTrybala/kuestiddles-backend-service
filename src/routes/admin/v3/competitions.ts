971bc746a8e15a4ca59a022849423cafd6121b45
Piotr Trybała <trybalapiotr4@gmail.com>
2026-06-02 22:40:54 +0200

    Implemented competitions for today

--- src/routes/admin/v3/competitions.ts
+++ src/routes/admin/v3/competitions.ts
@@ -1,1 +1,7 @@
 // `/:id{${UUID_PATTERN}}`
+
+// Competition router - /competitions
+
+// Group router - /competitions/:competitionId/groups
+
+// Invites router - /competitions/:competitionId/invites
